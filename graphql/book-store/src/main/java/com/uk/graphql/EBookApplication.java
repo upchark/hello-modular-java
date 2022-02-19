8efbf3a9e91569d3abe16613f5a0abdcac8dd5f5
upchark <patel.upchar@gmail.com>
2022-02-19 19:56:06 +0530

    added apps

---
+++ graphql/book-store/src/main/java/com/uk/graphql/EBookApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.uk.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pranika
+ *
+ */
+@SpringBootApplication
+public class EBookApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(EBookApplication.class, args);
+
+	}
+
+}
